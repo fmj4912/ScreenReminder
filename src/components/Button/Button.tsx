6683826a66ac8c6453b6b55334245f573f3f6ce6
Fredrik Josefsson <frjo1904@student.miun.se>
2022-10-23 21:19:03 +0200

    add some css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import styles from './Button.module.scss';
 import { Button} from '@mui/material';
-import { FilePresent, ThirtyFpsOutlined } from '@mui/icons-material';
 import soundEffect from '../../assets/test.mp3';
 
 
@@ -20,39 +19,34 @@ const ButtonComponent: FC<ButtonProps> = () => {
 	const [intervalKeeper, setIntervalKeeper] = useState<NodeJS.Timer>(null);
 	const timeInSeconds= useRef<number>(0);
 	const audioPlayer = useMemo<HTMLAudioElement>(() => new Audio(soundEffect), []);
-	
-
-	
-
 	const timeObjectStart = useMemo<TimeObj>(() => {return{
 		"h":0,
 		"m":0,
 		"s":0
-}},[]);
+	}},[]);
 	const [timeObject, setTimeObject] = useState<TimeObj>(timeObjectStart);
 	
-			
 	const secondsToTime = useCallback((secs) =>{
-		let hours = Math.floor(secs / (60 * 60));
-	
-		let divisor_for_minutes = secs % (60 * 60);
-		let minutes = Math.floor(divisor_for_minutes / 60);
-	
-		let divisor_for_seconds = divisor_for_minutes % 60;
-		let seconds = Math.ceil(divisor_for_seconds);
+	let hours = Math.floor(secs / (60 * 60));
 
-		if(minutes % 59 === 0 && seconds === 59 && minutes !== 0){
-			console.log("Time to play");
-			audioPlayer.play();
-		}
-	
-		let obj = {
-			"h": hours,
-			"m": minutes,
-			"s": seconds
-		};
-		return obj;
-	  },[audioPlayer]);
+	let divisor_for_minutes = secs % (60 * 60);
+	let minutes = Math.floor(divisor_for_minutes / 60);
+
+	let divisor_for_seconds = divisor_for_minutes % 60;
+	let seconds = Math.ceil(divisor_for_seconds);
+
+	if(minutes % 59 === 0 && seconds === 59 && minutes !== 0){
+		audioPlayer.play();
+	}
+
+	let obj = {
+		"h": hours,
+		"m": minutes,
+		"s": seconds
+	};
+
+	return obj;
+	},[audioPlayer]);
 
 	const timerHandler = useCallback(() => {
 		timeInSeconds.current = ++timeInSeconds.current;
@@ -79,26 +73,21 @@ const ButtonComponent: FC<ButtonProps> = () => {
 		timeInSeconds.current = 0;
 	}
 
-
-
-	  const timeFormatter= () =>{
-		let formattedString = timeObject.h > 9 ? timeObject.h + ":" : "0" + timeObject.h + ":";
-		formattedString += timeObject.m > 9 ? timeObject.m + ":" : "0" + + timeObject.m + ":";
-		formattedString += timeObject.s > 9 ? timeObject.s + ":" : "0" + + timeObject.s;
-		return formattedString;
-		
-	  }
-
+	const timeFormatter= () =>{
+	let formattedString = timeObject.h > 9 ? timeObject.h + ":" : "0" + timeObject.h + ":";
+	formattedString += timeObject.m > 9 ? timeObject.m + ":" : "0" + + timeObject.m + ":";
+	formattedString += timeObject.s > 9 ? timeObject.s + ":" : "0" + + timeObject.s;
+	
+	return formattedString;
+	}
 	
 	useEffect(() => {
-		console.log("yuse: " + enabled);
 		setButton(enabled ? <Button variant="contained" onClick={startTimer}>Stop</Button> : <Button variant="outlined" onClick={startTimer}>Start</Button>);
-}	,[enabled, startTimer]);
-
+	},[enabled, startTimer]);
 
 	return (
 		<>
-		<div className="outer-container">
+		<div className="outer-container" style={styles}>
 		<div className = "timer">{timeFormatter()}</div>
 		<div>
 			{button}
@@ -108,7 +97,5 @@ const ButtonComponent: FC<ButtonProps> = () => {
 	</>
 	);
 };
-
-
 
 export default ButtonComponent;
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,16 @@ module.exports = {
     entry: './index.js',
     resolve: {
         extensions: ['', '.js', '.jsx', '.tsx']
-    }
+    },
+    exclude: [
+        /\.html$/,
+        /\.(js|jsx)$/,
+        /\.css$/,
+        /\.json$/,
+        /\.svg$/,
+        /\.scss$/, //Add this line
+    ],
+    test: /\.scss$/,
+    loaders: ["style", "css", "sass"]
+    
 }
