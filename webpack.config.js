560ef31eb95c1feae966634387f8b0c24563808e
Fredrik Josefsson <frjo1904@student.miun.se>
2022-10-23 23:57:12 +0200

    Updates

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    entry: './index.js',
-    resolve: {
-        extensions: ['', '.js', '.jsx', '.tsx']
-    },
-    test: /\.scss$/,
-    loaders: ["style", "css", "sass"]
-    
-}
